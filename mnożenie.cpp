f95c08be8a950b5a56dad3af07d63377bdb19624
Arystokrata <krzysztoforosek@interia.pl>
2019-01-21 19:19:09 +0100

    KrzysztofJobczyński

---
+++ mnożenie.cpp
@@ -0,0 +1,12 @@
+int mnożenie()
+
+{
+int a,b,c;
+cout << "Podaj pierwszą liczbę" << a;
+cin >> a;
+cout << "Podaj drugą liczbę" <<b;
+cin >> b;
+c=a+b;
+cout << "Wynik mnożenia: " << c;
+return c;
+}
