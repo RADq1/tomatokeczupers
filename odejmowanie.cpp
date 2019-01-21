2cc25bf0b3c005c075a33dcc8d16a793c86a1a77
Arystokrata <krzysztoforosek@interia.pl>
2019-01-21 18:46:25 +0100

    KrzysztofJobczyński

---
+++ odejmowanie.cpp
@@ -0,0 +1,11 @@
+int odejmowanie()
+
+{
+int a,b,c;
+cout << "Podaj pierwszą liczbę" << a << endl;
+cin >> a;
+cout << "Podaj drugą liczbę" << b;
+cin >> b;
+c=a-b;
+cout << "Wynik dodawania: << c;
+return c;
